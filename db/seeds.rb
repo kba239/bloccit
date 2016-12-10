9c52f8d3f77535201c71e814c24de0e171c4203c
kba239 <adams.kate.b@gmail.com>
2016-12-10 13:46:54 -0500

    Assignment 20 Rails More CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,35 @@
 require 'random_data'
 
  # Create Posts
- 50.times do
+50.times do
 
-   Post.create!(
+  Post.create!(
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
+end
+posts = Post.all
 
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
-   )
- end
- posts = Post.all
+# Create Comments
 
- # Create Comments
+100.times do
+  Comment.create!(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
 
- 100.times do
-   Comment.create!(
+# Create Questions
 
-     post: posts.sample,
-     body: RandomData.random_paragraph
-   )
- end
+100.times do
+  Question.create!(
+    title:  RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    resolved: false
+  )
+end
 
- puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
