25ee9a0ac2a845ef6cd82f79ba7e3702ef88fb29
kba239 <adams.kate.b@gmail.com>
2016-12-10 19:49:40 -0500

    Checkpoint 22 Rails Topics and Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,35 @@
 require 'random_data'
 
- # Create Posts
- 50.times do
+# Create Topics
+15.times do
+  Topic.create!(
+    name:         RandomData.random_sentence,
+    description:  RandomData.random_paragraph
+  )
+end
+topics = Topic.all
 
-   Post.create!(
+# Create Posts
+50.times do
+  Post.create!(
+    topic:  topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
+end
 
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
-   )
- end
- posts = Post.all
+posts = Post.all
 
- # Create Comments
+# Create Comments
 
- 100.times do
-   Comment.create!(
+100.times do
+  Comment.create!(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
 
-     post: posts.sample,
-     body: RandomData.random_paragraph
-   )
- end
-
- puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+puts "Seed finished"
+puts "#{Topic.count} topics created"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
