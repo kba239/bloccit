35c842673bd55b5248e92046d7cbcfa33943f859
kba239 <adams.kate.b@gmail.com>
2016-12-15 13:37:05 -0500

    WIP Assignment 22 RailsTopics and Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,6 +29,17 @@ posts = Post.all
   )
 end
 
+# Create SponsoredPosts
+50.times do
+  SponsoredPost.create!(
+    topic:  topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
+end
+
+sponsored_posts = SponsoredPost.all
+
 puts "Seed finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
---
+++ spec/models/sponsored_post_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe SponsoredPost, type: :model do
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  let(:sponsored_post) { topic.sponsored_post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+
+  it { is_expected.to belong_to(:topic) }
+
+  describe "attributes" do
+    it "has title and body attributes" do
+      expect(sponsored_post).to have_attributes(title: title, body: body)
+    end
+  end
+end
